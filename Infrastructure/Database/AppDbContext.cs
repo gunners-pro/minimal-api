d5365e82985b312e79e2935c0e61ff38ba69a58e
Fabricyo Barreto <bass.barreto@gmail.com>
2024-09-02 21:23:22 -0300

    chore: add migrations

--- Infrastructure/Database/AppDbContext.cs
+++ Infrastructure/Database/AppDbContext.cs
@@ -6,4 +6,17 @@ namespace minimal_api.Infrastructure.Database;
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Administrator> Administrators { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Administrator>().HasData(
+            new Administrator
+            {
+                Id = 1,
+                Email = "administrador@teste.com",
+                Password = "123456",
+                Profile = "admin"
+            }
+        );
+    }
 }
